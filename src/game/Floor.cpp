8a4153b16849dcda1783d4376209547cafd1117b
Ciachociech <bastanator.rex@gmail.com>
2024-02-17 22:02:32 +0100

    add new sprites

--- src/game/Floor.cpp
+++ src/game/Floor.cpp
@@ -2,7 +2,7 @@
 
 namespace game {
 	
-Floor::Floor(int counter) : SolidObject("floor" + std::to_string(counter), "assets/sprites/WIP32x32.png") {}
+Floor::Floor(int counter) : SolidObject("floor" + std::to_string(counter), "assets/sprites/stone-test.png") {}
 
 void Floor::update() {
 	if (!isHitboxSet) {
