f42a417d1fb127dfac78061204746bef68f4e9f0
Ciachociech <bastanator.rex@gmail.com>
2024-02-18 10:18:31 +0100

    fixup! feat: introduce beholder

--- src/game/Beholder.cpp
+++ src/game/Beholder.cpp
@@ -15,7 +15,7 @@ Beholder::Beholder(int counter, sf::Vector2f position) : SolidObject("beholder" 
 	
 	rayHitboxes.push_back(sf::FloatRect(raySprite.getPosition().x + 8, raySprite.getPosition().y, 14, 41));
 	rayHitboxes.push_back(sf::FloatRect(raySprite.getPosition().x + 8, raySprite.getPosition().y + 41, 20, 47));
-	rayHitboxes.push_back(sf::FloatRect(raySprite.getPosition().x + 8, raySprite.getPosition().y + 47, 26, 40));
+	rayHitboxes.push_back(sf::FloatRect(raySprite.getPosition().x + 8, raySprite.getPosition().y + 88, 26, 40));
 }
 
 void Beholder::update() {
